d4bfaf963f14e6728eb44bd5a85e779af7e2c8d1
Hani <smhanizaidi@gmail.com>
2018-05-30 17:17:37 +0200

    messages

--- venv/myproject/pokerApp/views.py
+++ venv/myproject/pokerApp/views.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.http import HttpResponse
 from django.shortcuts import render
+
+def index(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 
 # Create your views here.
